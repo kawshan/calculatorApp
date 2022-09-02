623bdfdda5859196f03aa6592cfef8acb5104745
kawshan <112684462+kawshan@users.noreply.github.com>
2022-09-02 12:28:57 +0530

    Initial commit

---
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.calculatorapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView number1;
+    private TextView number2;
+    private TextView ansView;
+    private EditText editnum1;
+    private EditText editnum2;
+    private Button addButton;
+    private Button subButton;
+    private Button mulButton;
+    private Button divButton;
+    private Button resetButton;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //buttons set
+
+        editnum1=findViewById(R.id.editnum1);
+        editnum2=findViewById(R.id.editnum2);
+        addButton=findViewById(R.id.addButton);
+        subButton=findViewById(R.id.subButton);
+        mulButton=findViewById(R.id.mulButton);
+        divButton=findViewById(R.id.divButton);
+        resetButton=findViewById(R.id.resetButton);
+        ansView=findViewById(R.id.ansView);
+
+    }
+    public void calculator(View view){
+
+
+    }
+//    public void addNum(View view){
+//    int num1=Integer.parseInt(editnum1.getText().toString());
+//    int num2=Integer.parseInt(editnum2.getText().toString());
+//    ansView.setText(String.valueOf(num1+num2));
+//
+//    }
+//
+//    public void subButton(View view){
+//        int num1=Integer.parseInt(editnum1.getText().toString());
+//        int num2=Integer.parseInt(editnum2.getText().toString());
+//        ansView.setText(String.valueOf(num1-num2));
+//
+//    }
+//
+//    public void mulButton(View view){
+//        int num1=Integer.parseInt(editnum1.getText().toString());
+//        int num2=Integer.parseInt(editnum2.getText().toString());
+//        ansView.setText(String.valueOf(num1*num2));
+//    }
+//
+//    public void divButton(View view){
+//        double num1=Double.parseDouble(editnum1.getText().toString());
+//        double num2=Double.parseDouble(editnum2.getText().toString());
+//        ansView.setText(String.valueOf(num1/num2));
+//    }
+//
+//    public void resetBtn(View view){
+//
+//    }
+
+}
